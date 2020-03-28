47e164cd173e2ac50907ad21170562e169c36312
Vinoth Michael Xavier <vinoth.michaelxavier@gmail.com>
2020-03-28 13:04:37 -0400

    assert DOM classes, create basic DOM structure

--- src/components/PremiumRecipeCard.vue
+++ src/components/PremiumRecipeCard.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>Premium Recipe Card Component Goes Here</div>
+  <div class="prc">
+
+    <div class="prc__cover">
+      <img class="prc__img" />
+      <div class="prc__favorite">
+        <!-- Heart Icon -->
+      </div>
+      <div class="prc__tag">
+        <!-- Trophy Icon --> <!-- Premium Recipe -->
+      </div>
+    </div>
+
+    <div class="prc__info">
+      <div class="prc__header">
+        <!-- Low Carb Thai Chicken Curry With Coconut Cauliflower Rice -->
+      </div>
+
+      <div class="prc__ratings">
+        <!-- 5 star icons --> <!-- number of ratings (200 ratings) -->
+      </div>
+
+      <div class="prc__duration">
+        <!-- timer icon --> <!-- duration (24 minutes) -->
+      </div>
+
+      <div class="prc__burn">
+        <!-- burn icon --> <!-- num calories burned (489 Calories) -->
+      </div>
+
+      <div class="prc__macros">
+        <!-- red icon --> <!-- carbs (20g) -->
+        <!-- blue icon --> <!-- protein (16g) -->
+        <!-- yellow icon --> <!-- yellow (6g) -->
+      </div>
+    </div>
+
+  </div>
 </template>
--- tests/unit/PremiumRecipeCard.spec.js
+++ tests/unit/PremiumRecipeCard.spec.js
@@ -1,3 +1,24 @@
-describe("PremiumRecipeCard.vue", () => {
-  /* Tests go here */
-});
+import { shallowMount } from '@vue/test-utils'
+import PremiumRecipeCard from '@/components/PremiumRecipeCard.vue'
+
+describe('PremiumRecipeCard.vue', () => {
+
+  it('component does render', () => {
+    const wrapper = shallowMount(PremiumRecipeCard)
+    expect(wrapper.attributes('class')).toBe('prc')
+
+    // Cover
+    expect(wrapper.find('.prc__cover').exists()).toBe(true)
+    expect(wrapper.find('.prc__img').exists()).toBe(true)
+    expect(wrapper.find('.prc__favorite').exists()).toBe(true)
+    expect(wrapper.find('.prc__tag').exists()).toBe(true)
+
+    // Info
+    expect(wrapper.find('.prc__info').exists()).toBe(true)
+    expect(wrapper.find('.prc__header').exists()).toBe(true)
+    expect(wrapper.find('.prc__ratings').exists()).toBe(true)
+    expect(wrapper.find('.prc__duration').exists()).toBe(true)
+    expect(wrapper.find('.prc__burn').exists()).toBe(true)
+    expect(wrapper.find('.prc__macros').exists()).toBe(true)
+  })
+})
